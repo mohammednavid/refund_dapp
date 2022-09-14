fd8c3d98455f992715534bc809c6203b3d5a4d45
mohammednavid <navidmohd5@gmail.com>
2022-09-15 01:24:27 +0530

    first commit

--- refund_dapp/src/actions/web3Actions.js
+++
@@ -1,7 +0,0 @@
-export const getUserAddress = async () => {
-  const accounts = await window.ethereum.request({
-    method: "eth_requestAccounts",
-  });
-  const accountAddress = accounts[0];
-  return accountAddress;
-};
